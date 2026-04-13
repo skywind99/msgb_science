50bbe48c224c3beacbfa6e143cf1549f8ad60a45
skywind99 <skywind99@naver.com>
2026-04-13 14:23:13 +0900

    add vercel config

---
+++ api/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import { createServer } from "http";
+import { registerRoutes } from "../server/routes";
+
+const app = express();
+const httpServer = createServer(app);
+
+declare module "http" {
+  interface IncomingMessage {
+    rawBody: unknown;
+  }
+}
+
+app.use(
+  express.json({
+    verify: (req, _res, buf) => {
+      req.rawBody = buf;
+    },
+  })
+);
+app.use(express.urlencoded({ extended: false }));
+
+const ready = registerRoutes(httpServer, app);
+
+export default async function handler(req: any, res: any) {
+  await ready;
+  app(req, res);
+}
